4c09a485b4fe25a911b8309c6c5484f11532fcfa
Tony Yuan <Tonyyuan982@gmail.com>
2023-01-04 21:47:24 -0800

    temp stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="App">
+      <div className="title">
+        <h1>HoloDash</h1>
+        <p>Are you ready to explore the HoloDash world?</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="post">
+        <h2>Humble Beginnings</h2>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR test
+          Hello and welcome to the first blog post for our indie game, HoloDash!
         </p>
+
+        <p>
+          We are excited to announce that development has officially begun on
+          our new game, which is a cyberpunk themed roguelite. While we haven't
+          decided on a specific genre for the game yet, we can tell you that it
+          will be fast-paced and action-packed.
+        </p>
+
+        <p>
+          HoloDash runs will have procedually generated layouts and power-ups
+          that will make each playthrough feel unique. Players will be able to
+          explore through neon-lit streets, taking on challenging enemies and
+          bosses as they progress through the game.
+        </p>
+
+        <p>
+          We have only just begun development on HoloDash, but we are already
+          hard at work on bringing it to life. We will be sharing updates on our
+          progress as well as sneak peeks at gameplay and artwork in the future,
+          so stay tuned!
+        </p>
+
+        <p>
+          We can't wait for you to join us on this journey and be a part of
+          bringing HoloDash to life.
+        </p>
+
+        <p>Sincerely, </p>
+        <p>The HoloDash Team</p>
+        <hr />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
