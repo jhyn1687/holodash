6c524ee85a8bf843b7e997d7ca466e37221e46eb
Tony Yuan <tonyyuan982@gmail.com>
2023-02-20 20:33:29 -0800

    devlog alpha

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Post from "./components/Post"
 import postList from "./posts.json"
-import {MdEmail as Email} from "react-icons/md"
-import {RiTwitterFill as Twitter} from "react-icons/ri"
-import {GrReddit as Reddit} from "react-icons/gr"
-import {AiFillInstagram as Instagram} from "react-icons/ai"
-import { FaKickstarterK as KickStarter } from "react-icons/fa"
+import { MdEmail as Email } from "react-icons/md"
+import { RiTwitterFill as Twitter } from "react-icons/ri"
+import { GrReddit as Reddit } from "react-icons/gr"
+import { AiFillInstagram as Instagram } from "react-icons/ai"
+import { FaKickstarterK as KickStarter, FaItchIo as ItchIo } from "react-icons/fa"
 import "./App.css";
 
 function App() {
@@ -16,11 +16,12 @@ function App() {
         <p>Are you ready to explore the HoloDash world?</p>
         <p id="Names">Kyle Santos, Ashwin Subbaraman, Kevin Lee, Eva Liu, Tony Yuan</p>
         <div className="social-icons">
-          <a href="https://www.figma.com/file/eiA0PbCl8szlKFU6XICSuH/Untitled?node-id=0%3A1"><KickStarter/></a>
           <a href="mailto:playholodash@gmail.com"><Email /></a>
           <a href="https://twitter.com/holodash"><Twitter /></a>
           <a href="https://www.reddit.com/user/PlayHoloDash"><Reddit /></a>
           <a href="https://www.instagram.com/holodash_"><Instagram /></a>
+          <a href="https://holodash.itch.io/holodash"><ItchIo /></a>
+          <a href="https://www.figma.com/file/eiA0PbCl8szlKFU6XICSuH/Untitled?node-id=0%3A1"><KickStarter/></a>
         </div>
       </div>
       <div className="content">
